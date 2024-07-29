b587be6060cc4445d8e993aace73a83dab912c2e
ilya-nuhi <92297205+ilya-nuhi@users.noreply.github.com>
2024-07-30 02:56:18 +0300

    Update ClickRotator.cs

--- Assets/Scripts/ClickRotator.cs
+++ Assets/Scripts/ClickRotator.cs
@@ -10,6 +10,7 @@ public class ClickRotator : MonoBehaviour
     private void OnMouseUpAsButton()
     {
         transform.Rotate(0, 0, -90);
+        // Changing rotation index of the tile
         m_Tile.currentRotation = (m_Tile.currentRotation + 1) % 4;
     }
 }
